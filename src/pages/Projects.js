e8980a24f0b7fdcda28452731d8579aa09e7bc8a
Grant Boicheff <gboicheff@gmail.com>
2021-10-13 01:27:15 -0400

    fix

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,18 +8,8 @@ function Projects() {
         <div className="Blog">
             <Container>
                 <Row className="justify-content-center" style={style}>
-                    <Col xs={10}>
-                    <Card className="bg-dark text-white">
-                    <Card.Img src="holder.js/100px270" alt="Card image" />
-                    <Card.ImgOverlay>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
-                        </Card.Text>
-                        <Card.Text>Last updated 3 mins ago</Card.Text>
-                    </Card.ImgOverlay>
-                    </Card>
+                    <Col xs={6}>
+                        <h1>Coming soon...</h1>
                     </Col>
                 </Row>
             </Container>
