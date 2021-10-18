ec85b55dec72503979e477a8a11acef081511cc6
Grant Boicheff <gboicheff@gmail.com>
2021-10-18 01:19:56 -0400

    added project

--- src/text/ProjectList.js
+++ src/text/ProjectList.js
@@ -5,7 +5,14 @@ const ProjectList = [
         link: "https://github.com/gboicheff/SpamEmailDetection",
         keywords: ["Python", "scikit-learn", "numpy", "pandas"]
 
-    },    
+    },
+    {
+        title: "Lisp Interpreter",
+        description: "A Lisp interpreter capable of running basic programs.  It supports variables, functions, loops, conditionals statements and more.",
+        link: "https://github.com/gboicheff/Lisp-Interpreter",
+        keywords: ["Python"]
+
+    },   
 ]
 
 
