c127c4bf99d4b596cf480e5b51e70d7948b6a34c
Grant Boicheff <gboicheff@gmail.com>
2021-10-19 17:54:39 -0400

    Update ProjectList.js

--- src/text/ProjectList.js
+++ src/text/ProjectList.js
@@ -1,12 +1,5 @@
 const ProjectList = [
     {
-        title: "Spam Email Detection",
-        description: "Extracting features from spam emails and comparing the performance of models in classifying them.  Also implemented a Gaussian Naive Bayes classifier from scratch.",
-        link: "https://github.com/gboicheff/SpamEmailDetection",
-        keywords: ["Python", "scikit-learn", "numpy", "pandas"]
-
-    },
-    {
         title: "Lisp Interpreter",
         description: "A Lisp interpreter capable of running basic programs.  It supports variables, functions, loops, conditionals statements and more.",
         link: "https://github.com/gboicheff/Lisp-Interpreter",
@@ -19,7 +12,14 @@ const ProjectList = [
         link: "https://github.com/gboicheff/RegistrationHelper",
         keywords: ["Javascript", "jQuery"]
 
-    },   
+    },
+    {
+        title: "Spam Email Detection",
+        description: "Extracting features from spam emails and comparing the performance of models in classifying them.  Also implemented a Gaussian Naive Bayes classifier from scratch.",
+        link: "https://github.com/gboicheff/SpamEmailDetection",
+        keywords: ["Python", "scikit-learn", "numpy", "pandas"]
+
+    },
 ]
 
 
