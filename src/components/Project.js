b22834e91a8a27640da03dc471097449018b4b64
Grant Boicheff <gboicheff@gmail.com>
2021-10-20 16:31:39 -0400

    updated project layout

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,4 @@
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Image} from 'react-bootstrap';
 import GithubIcon from "../imgs/GitHub-Mark-Light-64px.png"
 
 function Project(props) {
@@ -6,15 +6,20 @@ function Project(props) {
         marginTop: "2vh",
         marginBottom: "2vh"
     }
+    const imageStyle = {
+        width: "32px",
+        height: "32px",
+        marginLeft: "1rem"
+    }
     return (
         <div className="Project" style={style}>
             <Card className="text-center" bg="dark" variant="dark">
             <Card.Body>
-                <Card.Title><b><h2>{props.title}</h2></b></Card.Title>
+                <Card.Title><b><h2>{props.title}<a href={props.link}><Image style={imageStyle} src={GithubIcon} fluid/></a></h2></b></Card.Title>
                 <Card.Text>
                 {props.description}
                 </Card.Text>
-                <Button variant="primary" href={props.link}>Check it out</Button>
+                {/* <Button variant="primary" href={props.link}>Check it out</Button> */}
             </Card.Body>
             <Card.Footer className="text-muted">{props.keywords.join(", ")}</Card.Footer>
             </Card>
