0aeb1fee063810a05f8956ef133fb316fa8687f7
Grant Boicheff <gboicheff@gmail.com>
2021-10-31 22:39:18 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -46,10 +46,10 @@ async function startApolloServer(typeDefs, resolvers){
   
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(join(__dirname, '../client/build')));
   
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+        res.sendFile(join(__dirname, '../client/build', 'index.html'));
     });
   }
 
