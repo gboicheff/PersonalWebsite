eb9e5f5f64e03e6a237d2a417f646725ee5c7e19
Grant Boicheff <gboicheff@gmail.com>
2021-11-05 15:33:02 -0400

    Update Bio.js

--- client/src/text/Bio.js
+++ client/src/text/Bio.js
@@ -1,7 +1,4 @@
-const Bio = `I'm a senior computer science major studying at the University of Florida.  \
-  I've previously interned as a software engineer intern at Walmart with a focus on the backend.
-  Currently, I work as a teaching assistant for a class called Programming Language Concepts.
-  I'm most interested in backend development but open to other roles.`
+const Bio = `I'm a senior computer science major studying at the University of Florida. Previously, I interned as a software engineer intern at Walmart with a focus on the backend. Now I work as a teaching assistant for a class called Programming Language Concepts. My career interests focus on backend development, but I'm open to other roles.`
 
 const Skills = {
   languages: ["Java", "Python", "Javscript, C++", "Julia"],
